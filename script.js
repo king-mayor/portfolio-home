4e1a7030bb704c981dcbb63eedd7e3b212a79562
king-mayor <99553727+king-mayor@users.noreply.github.com>
2022-12-09 13:30:29 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const toTop = document.querySelector(".to-top");
+window.addEventListener("scroll", function () {
+    if(window.pageYOffset > 100){
+        toTop.style.display = "block";
+    }else{
+        toTop.style.display = "none";
+    }
+})
+
